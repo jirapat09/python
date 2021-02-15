5fc4ae6dc9eac3e345562620d9fecc001811fa1b
jirapat thongkhamwan <jirapat.th@kkumail.com>
2021-02-16 01:12:42 +0700

    up

--- week6/work6.py
+++ week6/work6.py
@@ -3,12 +3,12 @@ import os
 conn = sqlite3.connect(r'D:\Jirapat_python\week6\work6.db')
 c = conn.cursor()
 
-"""c.execute ('''CREATE TABLE students(id integer PRIMARY KEY AUTOINCREMENT,
+c.execute ('''CREATE TABLE students(id integer PRIMARY KEY AUTOINCREMENT,
     name varchar(50) NOT NULL,
     email varchar(50) NOT NULL,
     gender varchar(10) NOT NULL,
     old varchar(10) NOT NULL,
-    year varchar(2) NOT NULL)''')"""
+    year varchar(2) NOT NULL)''')
 
 def menu():
     global choice
