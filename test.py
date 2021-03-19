179294f122a71a9bd9a1f64d8092e6104234a767
jirapat thongkhamwan <jirapat.th@kkumail.com>
2021-03-19 17:38:16 +0700

    update

--- test.py
+++ test.py
@@ -1,17 +1,4 @@
-from tkinter import *
-import tkinter as tk
-from PIL import Image, ImageTk
-
-gui = Tk()
-gui.option_add("*Font", "courier 20")
-gui.geometry("700x500")
-gui.title("โปรแกรมกู้เงินพอมเจมส์")
-Label (gui,text="กรุณาเลือกกดตัวเลือกต่อไปนี้",bg='red3').pack(fill=X)
-Button (gui,text ="สมัครสมาชิก").place(relx=0.05,rely=0.2)
-Button (gui,text ="เข้าสู่ระบบ").place(relx=0.05,rely=0.36)
-Button (gui,text ="ออกจากโปรแกรม").place(relx=0.05,rely=0.85)
-
-photo = PhotoImage(file="POK.jpg")
-Label (gui, image=photo, borderwidth=0).pack()
-
-gui.mainloop()
+try:
+    value=int(input("Type a number:"))
+except ValueError:
+    print("โปรดใส่แค่ตัวเลข")
